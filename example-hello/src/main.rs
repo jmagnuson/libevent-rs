36c27e4e808c487e53c3612d1749db75a030a40b
Jon Magnuson <jon.magnuson@gmail.com>
2020-04-12 19:51:54 -0500

    add add_interval function

--- example-hello/src/main.rs
+++ example-hello/src/main.rs
@@ -1,8 +1,18 @@
 
 use std::time::Duration;
 use libevent::Libevent;
+use libevent_sys;
 
 pub mod ffi;
+
+use std::os::raw::{c_int, c_short, c_void};
+
+type EvutilSocket = c_int;
+type EventCallbackFn = extern "C" fn(EvutilSocket, c_short, *mut c_void);
+
+extern "C" fn hello_callback(fd: EvutilSocket, event: c_short, ctx: *mut c_void) {
+    println!("Rust callback says hello");
+}
 
 fn main() {
     println!("Hello, world!");
@@ -13,6 +23,25 @@ fn main() {
     let _ = unsafe { libevent.with_base(|base| {
         ffi::helloc_init(base)
     })};
+
+    let ev = unsafe { libevent.base().event_new(
+        None,
+        libevent_sys::EV_PERSIST as c_short,
+        hello_callback,
+        unsafe {std::mem::transmute(std::ptr::null::<c_void>()) },
+    ) };
+
+    let _ = unsafe {
+        libevent.base().event_add(ev, Duration::from_secs(2))
+    };
+    /*let _ev = unsafe { libevent.with_base(|base| {
+        base.event_new()
+    })};*/
+
+    let _ = libevent.add_interval(
+        Duration::from_secs(6),
+        || { println!("whoaaa"); }
+    );
 
     let libevent_ref = &libevent;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
 #![allow(dead_code)]
 
 use std::io;
-use std::os::raw::c_int;
+use std::os::raw::{c_int, c_short, c_void};
 use std::time::Duration;
 use libevent_sys;
+
+type EvutilSocket = c_int;
+
+type EventCallbackFn = extern "C" fn(EvutilSocket, c_short, *mut c_void);
+
+/// Gets used as the boxed context for `EXternCallbackFn`
+struct EventCallbackWrapper {
+    inner: Box<dyn FnMut()>,
+}
+
+extern "C" fn handle_wrapped_callback(fd: EvutilSocket, event: c_short, ctx: *mut c_void) {
+    let cb_ref = unsafe {
+        let cb: *mut EventCallbackWrapper = std::mem::transmute( ctx );
+        let _cb_ref: &mut EventCallbackWrapper = &mut *cb;
+        _cb_ref
+    };
+
+    (cb_ref.inner)()
+}
+
 
 fn to_timeval(duration: Duration) -> libevent_sys::timeval {
     #[cfg(any(target_os = "linux", target_os = "android", target_os = "solaris"))]
@@ -30,6 +50,7 @@ pub struct EventBase {
 unsafe impl Send for EventBase {}
 unsafe impl Sync for EventBase {}
 
+/// The handle that abstracts over libevent's API in Rust.
 impl EventBase {
     pub fn new() -> Result<Self, io::Error> {
         let base = unsafe {
@@ -66,6 +87,45 @@ impl EventBase {
             libevent_sys::event_base_loopexit(self.base, tv_cast) as i32
         }
     }
+
+    pub fn event_new(
+        //&mut self,
+        & self,
+        fd: Option<EvutilSocket>,
+        flags: c_short,
+        callback: EventCallbackFn,
+        callback_ctx: *mut c_void,
+    ) -> *mut libevent_sys::event {
+        let fd: EvutilSocket = if let Some(fd_) = fd {
+            // Actual fd
+            fd_
+        } else {
+            // Timer
+            -1
+        };
+
+        unsafe {
+            libevent_sys::event_new(
+                self.as_inner_mut(),
+                fd,
+                flags,
+                Some(callback),
+                callback_ctx,
+            )
+        }
+    }
+
+    pub fn event_add(
+        //&mut self,
+        & self,
+        event: *mut libevent_sys::event,
+        timeout: Duration,
+    ) -> c_int {
+        let tv = to_timeval(timeout);
+        unsafe {
+            libevent_sys::event_add(event, &tv)
+        }
+    }
 }
 
 pub struct Libevent {
@@ -88,7 +148,7 @@ impl Libevent {
         f(self.base.as_inner_mut())
     }
 
-    pub(crate) unsafe fn base(&self) -> &EventBase {
+    pub/*(crate)*/ unsafe fn base(&self) -> &EventBase {
         &self.base
     }
 
@@ -105,6 +165,25 @@ impl Libevent {
     pub fn loop_timeout(&self, timeout: Duration) -> bool {
         let _retval = self.base.loopexit(timeout);
         let _retval = self.base.loop_(0i32);
+
+        true
+    }
+
+    pub fn add_interval<F: FnMut() + 'static>(&self, interval: Duration, cb: F) -> bool {
+        let cb_wrapped = Box::new(EventCallbackWrapper {
+            inner: Box::new(cb)
+        });
+
+        let ev = unsafe { self.base().event_new(
+            None,
+            libevent_sys::EV_PERSIST as c_short,
+            handle_wrapped_callback,
+            unsafe {std::mem::transmute(cb_wrapped) },
+        ) };
+
+        let _ = unsafe {
+            self.base().event_add(ev, Duration::from_secs(2))
+        };
 
         true
     }
