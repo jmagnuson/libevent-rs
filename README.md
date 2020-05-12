543d1794256ee14c6781b323d9c9deca786dd70b
Jon Magnuson <jon.magnuson@gmail.com>
2020-05-11 22:02:11 -0500

    Initial commit of `bundled` build feature
    Adds `cmake` as a build-dependency in order to do builds directly from
    libevent source. This feature flag also serves as a toggle between
    building and the current method of discovery using pkg-config. This is
    done to try and simplify the logical complexity of the build script.

--- README.md
+++ README.md
@@ -4,6 +4,19 @@ Rust bindings to the [libevent] async I/O framework.
 
 Check out the [hacking] branch for now.
 
+### Sysem Requirements
+
+* `libclang` is required by [bindgen] which is used to generate the Rust
+  bindings. See [bindgen requirements] for more information. Also ensure that
+  `LIBCLANG_PATH` is set, as some systems do not do so by default.
+
+* `cmake` if self-building via the `bundled` feature. The current bundled
+  release is `release-2.1.11-stable`.
+
+* `pkg-config` if not self-building via the `bundled` feature.
+
 
 [libevent]: https://libevent.org/
 [hacking]: https://github.com/jmagnuson/libevent-rs/tree/hacking
+[bindgen]: https://crates.io/crates/bindgen
+[bindgen requirements]: https://rust-lang.github.io/rust-bindgen/requirements.html
--- libevent-sys/build.rs
+++ libevent-sys/build.rs
@@ -1,18 +1,117 @@
 use std::env;
 use std::path::PathBuf;
 
-use bindgen;
-use pkg_config;
+#[cfg(feature = "bundled")]
+fn build_libevent(libevent_path: impl AsRef<std::path::Path>) -> PathBuf {
+    let mut config = cmake::Config::new(libevent_path);
+
+    // TODO: Or just both and decide elsewhere?
+    if cfg!(feature = "static") {
+        config.define("EVENT__LIBRARY_TYPE", "STATIC");
+    } else {
+        config.define("EVENT__LIBRARY_TYPE", "SHARED");
+    }
+
+    // Disable unnecessary dev-only features
+    {
+        config.define("EVENT__DISABLE_BENCHMARK", "ON")
+            .define("EVENT__DISABLE_TESTS", "ON")
+            .define("EVENT__DISABLE_REGRESS", "ON")
+            .define("EVENT__DISABLE_SAMPLES", "ON");
+
+        // TODO: Address policy warnings for cross-compilation?
+        /*config.define("CMAKE_POLICY_DEFAULT_CMP0056", "NEW")
+            .define("CMAKE_POLICY_DEFAULT_CMP0066", "NEW")*/
+    }
+
+    let dst = config.build();
+
+    println!("cargo:rustc-link-search={}/lib", dst.display());
+
+    // Library 'event' is considered deprecated, so link each sub-component
+    // individually.
+    println!("cargo:rustc-link-lib=static=event_core");
+    println!("cargo:rustc-link-lib=static=event_extra");
+
+    println!("cargo:include={}/include", dst.display());
+
+    dst
+}
+
+#[cfg(not(feature = "bundled"))]
+fn run_pkg_config() -> Option<Vec<String>> {
+    use std::collections::HashSet;
+
+    let mut pkg = pkg_config::Config::new();
+    pkg.cargo_metadata(true)
+        .atleast_version("2")
+        .statik(cfg!(feature = "static"));
+
+    let mut include_paths = HashSet::new();
+
+    if let Ok(mut lib) = pkg.probe("libevent_core") {
+        include_paths.extend(lib.include_paths.drain(..));
+    } else {
+        return None;
+    }
+
+    {
+        let mut lib = pkg.probe("libevent_extra").unwrap();
+        include_paths.extend(lib.include_paths.drain(..));
+    }
+
+    let include_paths = include_paths.drain().map(|path| {
+        let path_s = path.into_os_string().into_string().unwrap();
+        println!("cargo:include={}", &path_s);
+        path_s
+    }).collect();
+
+    Some(include_paths)
+}
+
+#[cfg(feature = "bundled")]
+fn find_libevent() -> Option<Vec<String>> {
+    use std::path::Path;
+    use std::process::Command;
+
+    if !Path::new("libevent/.git").exists() {
+        Command::new("git").args(&["submodule", "update", "--init"])
+            .status().expect("Running `git submodule init` failed.");
+    } else {
+        Command::new("git").args(&["submodule", "update", "--recursive"])
+            .status().expect("Running `git submodule update` failed.");
+    }
+
+    Some(vec![format!("{}/include", build_libevent("libevent").display())])
+}
+#[cfg(not(feature = "bundled"))]
+fn find_libevent() -> Option<Vec<String>> {
+    run_pkg_config()
+}
 
 fn main() {
-    // Use pkg-config to find and link libevent
-    pkg_config::Config::new()
-        .atleast_version("2")
-        .statik(cfg!(feature = "static"))
-        .probe("libevent")
-        .unwrap();
+    println!("cargo:rerun-if-changed=libevent");
+    println!("cargo:rerun-if-changed=wrapper.h");
 
-    let bindings = bindgen::Builder::default()
+    let target = env::var("TARGET").unwrap();
+    let host = env::var("HOST").unwrap();
+
+    let include_paths = find_libevent()
+        .expect("No include paths for libevent found");
+
+    let mut builder = bindgen::Builder::default();
+
+    if target != host {
+        builder = builder.clang_arg("-target");
+        builder = builder.clang_arg(target);
+    }
+
+    // Let bindgen know about all include paths that were found.
+    for path in include_paths {
+        builder = builder.clang_arg(format!("-I{}", path));
+    }
+
+    let bindings = builder
         .header("wrapper.h")
         // Enable for more readable bindings
         // .rustfmt_bindings(true)
