7d3165d2f3641a6ba3eccc41a4be33ae1f28c382
klebz <92199902+klebz@users.noreply.github.com>
2022-06-09 13:52:06 -0400

    Update wrapper.h

--- libevent-sys/wrapper.h
+++ libevent-sys/wrapper.h
@@ -1,5 +1,15 @@
 #include <event.h>
 
+#include <event2/event.h>       // The primary libevent header
+#include <event2/thread.h>      // Functions for use by multithreaded programs
+#include <event2/buffer.h>      // and event2/bufferevent.h Buffer management for network reading and writing
+#include <event2/bufferevent.h> // and event2/bufferevent.h Buffer management for network reading and writing
+#include <event2/util.h>        // Utility functions for portable nonblocking network code
+#include <event2/dns.h>         // Asynchronous DNS resolution
+#include <event2/http.h>        // An embedded libevent-based HTTP server
+#include <event2/rpc.h>         // A framework for creating RPC servers and clients
+
+
 #ifdef EVENT__HAVE_OPENSSL
 #include <event2/bufferevent_ssl.h>
 #endif
