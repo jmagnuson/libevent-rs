decc59b4bfd2ca26f587d2d066c1e95632cb13f8
Thomas Klebanoff <92199902+klebz@users.noreply.github.com>
2023-01-25 23:40:07 -0500

    Update wrapper.h (#19)
    * Update wrapper.h

--- libevent-sys/wrapper.h
+++ libevent-sys/wrapper.h
@@ -1,5 +1,12 @@
 #include <event.h>
 
+
+#include <event2/util.h>        // Utility functions for portable nonblocking network code
+#include <event2/dns.h>         // Asynchronous DNS resolution
+#include <event2/http.h>        // An embedded libevent-based HTTP server
+#include <event2/rpc.h>         // A framework for creating RPC servers and clients
+
+
 #ifdef EVENT__HAVE_OPENSSL
 #include <event2/bufferevent_ssl.h>
 #endif
